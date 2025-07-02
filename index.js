6eca2a0cdee4e3ff60ba85719e217a8f0844e536
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-07-02 12:59:25 +0530

    edit and delete feacher added

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
-const addButton = document.querySelector('.addProduct')
-const form = document.querySelector('form')
-const button = document.querySelector('.submitBtn')
-const crossBtn = document.querySelector('.cross')
-const cancelBtn = document.querySelector('.cancel')
-const productList = document.querySelector('#productList')
+const addButton = document.querySelector(".addProduct");
+const editButton = document.querySelectorAll(".editProductBtn");
+// console.log(editButton);
 
-addButton.addEventListener('click',()=>{
-form.style.display = 'block'
-productList.style.opacity = 0.2
-})
+const FormToAdd = document.querySelector("#FormToAdd");
+const FormToEdit = document.querySelector("#FormToEdit");
 
-crossBtn.addEventListener('click',()=>{
-  form.style.display = 'none'
-  productList.style.opacity = 1
-})
-productList.addEventListener('click',()=>{
-  form.style.display = 'none'
-   productList.style.opacity = 1
-})
-cancelBtn.addEventListener('click',()=>{
-  form.style.display = 'none'
-   productList.style.opacity = 1
-})
+const crossBtn = document.querySelectorAll(".cross");
+const cancelBtn = document.querySelectorAll(".cancel");
 
+const productList = document.querySelector("#productList");
+
+const product = document.querySelectorAll(".product");
+
+const deleteButton = document.querySelectorAll(".deleteProductBtn");
+console.log(deleteButton);
+
+// buttons to show Form
+addButton.addEventListener("click", () => {
+  FormToAdd.style.display = "block";
+  productList.style.opacity = 0.2;
+});
+
+editButton.forEach((element) => {
+  element.addEventListener("click", () => {
+    FormToEdit.style.display = "block";
+    productList.style.opacity = 0.2;
+    console.log("Edit clicked");
+  });
+});
+
+// buttons to remove Form
+function removeForm(form) {
+  form.style.display = "none";
+  productList.style.opacity = 1;
+}
+crossBtn[0].addEventListener("click", () => removeForm(FormToAdd));
+crossBtn[1].addEventListener("click", () => removeForm(FormToEdit));
+cancelBtn[0].addEventListener("click", () => removeForm(FormToAdd));
+cancelBtn[1].addEventListener("click", () => removeForm(FormToEdit));
+
+// click outside the form
+productList.addEventListener("click", () => removeForm(FormToAdd));
+
+// delete the product
+for (let i = 0; i < product.length; i++) {
+  deleteButton[i].addEventListener("click", () => {
+    product[i].style.display = "none";
+  });
+}
